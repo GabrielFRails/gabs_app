60ece5867c151110e41880ea9a353341a88654b1
GabrielFRails <gabriel.estudy.reis@gmail.com>
2024-01-20 12:12:12 -0300

    Create more view and link urls to they

--- polls/views.py
+++ polls/views.py
@@ -5,3 +5,15 @@ def index(request):
 # {
     return HttpResponse("Hello friend, you're at the polls index. Wellcome!")
 # }
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}")
+
+def results(request, question_id):
+# {
+    response = f"You're looking at the results of question {question_id}."
+    return HttpResponse(response % question_id)
+# }
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
